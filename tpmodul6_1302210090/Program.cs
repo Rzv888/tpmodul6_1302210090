aa277429c2993c4bbbaa1c4909176fdc7be71f3e
Rzv888 <udinamin445@gmail.com>
2023-03-27 02:20:07 +0700

    implementasi design by contract

--- tpmodul6_1302210090/Program.cs
+++ tpmodul6_1302210090/Program.cs
@@ -1,8 +1,35 @@
 ﻿using tpmodul6_1302210090;
 
 class MainClass{
-    static void Main(){
+    static void Main()
+    {
         SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - [NAMA_PRAKTIKAN]");
+        try{
+            video.IncreasePlayCount(-1);
+        }
+        catch (ArgumentOutOfRangeException ex){
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
+        try{
+            video.IncreasePlayCount(15000000);
+        }
+        catch (ArgumentOutOfRangeException ex){
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        try{
+            for (int i = 0; i < 1000000; i++)
+            {
+                checked
+                {
+                    video.IncreasePlayCount(10000);
+                }
+            }
+        }
+        catch (OverflowException ex){
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
         video.PrintVideoDetails();
     }
 }
--- tpmodul6_1302210090/SayaTubeVideo.cs
+++ tpmodul6_1302210090/SayaTubeVideo.cs
@@ -2,20 +2,28 @@
 
 namespace tpmodul6_1302210090
 {
-    internal class SayaTubeVideo{
+    internal class SayaTubeVideo
+    {
         private int id;
         private string title;
         private int playCount;
-        public SayaTubeVideo(string title){
+        public SayaTubeVideo(string title)
+        {
             Random rand = new Random();
             this.id = rand.Next(10000, 99999);
             this.title = title;
             this.playCount = 0;
         }
-        public void IncreasePlayCount(int count){
+        public void IncreasePlayCount(int count)
+        {
+            if (count <= 0 || count > 10000000)
+            {
+                throw new ArgumentOutOfRangeException("count", "Input penambahan play count harus berada di antara 1 dan 10.000.000.");
+            }
             this.playCount += count;
         }
-        public void PrintVideoDetails(){
+        public void PrintVideoDetails()
+        {
             Console.WriteLine("Video ID: " + this.id);
             Console.WriteLine("Title: " + this.title);
             Console.WriteLine("Play count: " + this.playCount);
